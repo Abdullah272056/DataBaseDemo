1a7ac0bbc796a53d80dde231f20d04bc190e00b7
Abdullah272056 <abdullah272056@gmail.com>
2020-03-04 19:34:57 +0600

    Add Semester Column

--- app/src/main/java/com/example/databasedemo/Query.java
+++ app/src/main/java/com/example/databasedemo/Query.java
@@ -9,7 +9,7 @@ public class Query {
 
     public static final String DataBaseName="student.db";
     public static final String TableName="student_details";
-    public static final int DataBaseVersion=1;
+    public static final int DataBaseVersion=3;
 
     public static final String ID="Id";
     public static final String NAME="Name";
@@ -18,6 +18,8 @@ public class Query {
     public static final String SHIFT="Shift";
     public static final String DEPARTMENT="Department";
     public static final String MOBILE="Mobile";
+    public static final String SEMESTER="Semester";
+
 
     public static final String CREATE_TABLE=" CREATE TABLE "+TableName+"("
             +ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "
@@ -26,10 +28,11 @@ public class Query {
             +ADDRESS+" TEXT, "
             +SHIFT+" TEXT, "
             +DEPARTMENT+" TEXT, "
-            +MOBILE+" TEXT "
+            +MOBILE+" TEXT, "
+            +SEMESTER+" TEXT "
             +")";
 
-
+    public static final String DROP_TABLE= " DROP TABLE IF EXISTS "+TableName;
 
 
 }
